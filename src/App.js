1f3c1635e5dcb556c5fff0ee6f25d667dc418cb7
abbasmortazavi <abbassmortazavi@gmail.com>
2026-07-13 12:26:26 +0330

    feat: create home and movie page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -21,11 +22,15 @@ function App() {
             <div className="list-group">
               <a href="#!" className="list-group-item list-group-item-action">Home</a>
               <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+              <a href="#!" className="list-group-item list-group-item-action">Genres</a>
+              <a href="#!" className="list-group-item list-group-item-action">Add Movie</a>
+              <a href="#!" className="list-group-item list-group-item-action">Manage Catalogue</a>
+              <a href="#!" className="list-group-item list-group-item-action">GraphQL</a>
             </div>
           </nav>
         </div>
         <div className="col-md-10">
-
+          <Home/>
         </div>
       </div>
     </div>
---
+++ src/components/Movie.js
@@ -0,0 +1,12 @@
+
+const Movie = () => {
+    return (
+        <div className="text-center">
+            <h2>Movies</h2>
+            <hr/>
+        </div>
+    );
+}
+
+
+export default Movie;
