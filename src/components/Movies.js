cbd8086c3ee5a76ed7711b23232ab53c14d3db70
abbasmortazavi <abbassmortazavi@gmail.com>
2026-07-16 11:15:43 +0330

    feat: complete movies page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,57 @@
+import { useEffect, useState } from "react";
 
 const Movies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        let moviesList = [
+            {
+                id: 1,
+                title: "new movie",
+                release_date: "2026-01-02",
+                runtime: 20,
+                mpa_rating: "R",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            },
+            {
+                id: 2,
+                title: "another movie",
+                release_date: "2026-01-02",
+                runtime: 20,
+                mpa_rating: "R",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }
+        ]
+        setMovies(moviesList);
+    }, [])
+
     return (
         <div className="text-center">
             <h2>Movies</h2>
             <hr/>
+
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Release Date</th>
+                        <th>Runtime</th>
+                        <th>MPA Rating</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => (
+                        <tr key={movie.id}>
+                            <td><a href={`/movies/${movie.id}/edit`}>{movie.title}</a></td>
+                            <td>{movie.release_date}</td>
+                            <td>{movie.runtime}</td>
+                            <td>{movie.mpa_rating}</td>
+                            <td>{movie.description}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
