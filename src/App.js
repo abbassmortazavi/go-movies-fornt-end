ce7c9d349059b7b7072d7ead95d11adb403f579c
abbasmortazavi <abbassmortazavi@gmail.com>
2026-07-17 10:43:02 +0330

    feat: implement login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
+
   return (
     <div className="container">
       <div className="row">
@@ -9,11 +12,17 @@ function App() {
         </div>
 
         <div className="col text-end">
-          <Link to="/login">
-            <span className="badge bg-success">Login</span>
-          </Link>
+          {
+            jwtToken === ""
+              ?
+              <Link to="/login">
+                <span className="badge bg-success">Login</span>
+              </Link>
+              : <a href="!#"><span className="badge bg-danger">Logout</span></a>
+          }
+
         </div>
-        <hr className="mb-3"/>
+        <hr className="mb-3" />
       </div>
 
       <div className="row">
@@ -23,14 +32,22 @@ function App() {
               <Link to="/" className="list-group-item list-group-item-action">Home</Link>
               <Link to="/movies" className="list-group-item list-group-item-action">Movies</Link>
               <Link to="/genres" className="list-group-item list-group-item-action">Genres</Link>
-              <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
-              <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
-              <Link to="/graphql" className="list-group-item list-group-item-action">GraphQL</Link>
+              {
+                jwtToken !== "" &&
+                <>
+                  <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
+                  <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
+                  <Link to="/graphql" className="list-group-item list-group-item-action">GraphQL</Link>
+                </>
+              }
+
             </div>
           </nav>
         </div>
         <div className="col-md-10">
-         <Outlet/>
+          <Outlet context={{
+            setJwtToken, jwtToken
+          }}/>
         </div>
       </div>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import Input from "./form/Input";
+import { useOutletContext } from "react-router-dom";
+
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {setJwtToken} = useOutletContext();
+
+    const handelSubmit = (event)=>{
+        event.preventDefault();
+        console.log("user/pass", email, password);
+        if (email === "jafar@yahoo.com"){
+            setJwtToken("abc");
+        }
+    }
     return (
-        <div className="text-center">
+        <div className="col-md-6 offset-3">
             <h2>Login</h2>
             <hr/>
+            <form onSubmit={handelSubmit}>
+                <Input
+                    type="email"
+                    name="email"
+                    title="Email Address"
+                    placeholder="Eneter Your Email!"
+                    className="form-control"
+                    outocomplete="email-new"
+                    onChange={(event)=> setEmail(event.target.value)}
+                />
+                 <Input
+                    type="password"
+                    name="password"
+                    title="Password"
+                    placeholder="Eneter Your Password!"
+                    className="form-control"
+                    outocomplete="password-new"
+                    onChange={(event)=> setPassword(event.target.value)}
+                />
+                <input type="submit" className="btn btn-primary" value="Send"/>
+            </form>
         </div>
     );
 }
